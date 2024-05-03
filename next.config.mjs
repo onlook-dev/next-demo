76c2ec9481acd6eeb3fc0be5762663a7732dfe9e
Kiet Ho <hoakiet98@gmail.com>
2024-05-03 14:42:21 -0400

    Add dashboard

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+import path from "path";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    swcPlugins: [["@onlook/nextjs", { root: path.resolve(".") }]],
+  },
+}
+
+export default nextConfig
