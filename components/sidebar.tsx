d3d20120ea1903a0db9648786c91030c5925ca39
Kiet Ho <hoakiet98@gmail.com>
2024-05-04 13:42:59 -0400

    Remove acme

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -7,13 +7,6 @@ export function Sidebar() {
         <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
             <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
                 <TooltipProvider>
-                    <Link
-                        className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
-                        href="dashboard"
-                    >
-                        <Package2Icon className="h-4 w-4 transition-all group-hover:scale-110" />
-                        <span className="sr-only">Acme Inc</span>
-                    </Link>
                     <Tooltip>
                         <TooltipTrigger asChild>
                             <Link
