4406a69dddd6a80ac85eb9e7b3bfbe2a140c5ed3
Kiet Ho <hoakiet98@gmail.com>
2024-07-01 15:34:31 -0400

    Add absolute path

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
-import path from "path";
 import { getCurrentCommit } from "@onlook/helpers/server";
+import path from "path";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
-    swcPlugins: [["@onlook/nextjs", { root: path.resolve("."), commit: getCurrentCommit() }]],
+    swcPlugins: [["@onlook/nextjs", { root: path.resolve("."), commit: getCurrentCommit(), absolute: true }]],
   },
 }
 
